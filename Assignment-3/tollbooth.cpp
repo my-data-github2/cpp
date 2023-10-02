98cb502c896cbb2277b144f00f0ff42697fa01d1
kmark15 <kunal15markam@gmail.com>
2023-10-02 14:15:27 +0530

    Assignment-3 done

--- Assignment-3/tollbooth.cpp
+++ Assignment-3/tollbooth.cpp
@@ -14,46 +14,54 @@ public:
         this->total_amt = 0;
     }
 
-    int payingCar(int total_car, double total_amt)
+    void payingCar(int total_car, double total_amt)
     {
         this->total_car = this->total_car + 1;
         this->total_amt = this->total_amt + 0.50;
     }
-    int nopayCar()
+    void nopayCar()
     {
-        return this->total_car = this->total_car + 1;
+     this->total_car = this->total_car + 1;
     }
 
-    int printOnConsole()
+    void  printOnConsole()
     {
-        cout << payingCar(1,4.2);
-        cout << nopayCar();
+      cout<<"Total cars "<<this->total_car <<endl;
+      cout<<"Total cash "<<this->total_amt<<endl;
+      cout<<"Total paying cars "<< this->total_car - (total_amt / 0.50)<<endl;
+      cout<<"Total Non Paying cars "<<(total_amt / 0.50)<<endl;
     }
 };
 
 int main()
 {
 
-    int choice;
+    int choice = -1;
     tollbooth t1;
 
-    cout << "0. Exit" << endl;
-    cout << "1. Number of Paying Cars: " << endl;
-    cout << "2. Number of toll paying cars: " << endl;
-    cout << "3. Print";
-
-    cout << "Enter the Choice:" << endl;
-    cin >> choice;
-    switch (choice)
+    while (choice != 0)
     {
-    case 1:
-        t1.payingCar(1,4.2);
-        break;
-    case 2:
-        t1.nopayCar();
-        break;
-    case 3:
-        t1.printOnConsole();
-        break;
+        cout << "0. Exit" << endl;
+        cout << "1. Number of Paying Cars: " << endl;
+        cout << "2. Number of toll paying cars: " << endl;
+        cout << "3. Print"<<endl;
+
+        cout << "Enter the Choice:" << endl;
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            t1.payingCar(1, 4.2);
+            break;
+        case 2:
+            t1.nopayCar();
+            break;
+        case 3:
+            t1.printOnConsole();
+            break;
+        default:
+            cout << "invalid choice"<<endl;
+        }
     }
+    return 0;
 }
